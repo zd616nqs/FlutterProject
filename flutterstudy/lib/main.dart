edc9e102fb80631e754bc016db07c61bccaaf775
牛青山 <qingshan.niu@ly.com>
2019-05-07 19:55:07 +0800

    2019年05月07日

--- flutterstudy/lib/main.dart
+++ flutterstudy/lib/main.dart
@@ -1,149 +1,62 @@
 import 'package:flutter/material.dart';
 
-// void main() => runApp(MyApp());
+// void main() => runApp(NQSTest());
+void main() => runApp(TextWidgetExample());
 
-void main() {
-  runApp(
-    MaterialApp( 
-      home:Scaffold(
-        appBar:AppBar(
-          backgroundColor: Colors.white,
-          title: Text('牛青山测试1'),
+class NQSTest extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: '牛青山示例',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('大标题'),
         ),
-        body: NQSTest1(),
-      )
-    )
-  );
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Text('hello world11'),
+              Text('hello world22'),
+              Text('hello world33'),
+            ],
+          )
+        ),
+      ),
+    );
+  }
 }
 
-class NQSTest1 extends StatelessWidget {
+//Text组件
+class TextWidgetExample extends StatelessWidget {
 
   @override
   Widget build(BuildContext context){
-    return Center(
-      child:Container(
-        color: Colors.green,
-        height: 200,
-        width: 200,
-        child:Column(
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            )
-          ],
-        )
-      )
-    );
-  }
-}
-
-
-
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.red,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            //文字
+            "我是牛青山，哈哈哈哈哈哈",
+            //对齐方式：left/right/center/justify/start/end
+            textAlign: TextAlign.justify,
+            //裁剪方式：clip直接裁剪文字   ellipsis裁剪文字后加... fade从上到下颜色渐变
+            overflow: TextOverflow.clip,
+            style: TextStyle(
+              //字号：最好为float型，int不报错
+              fontSize: 20,
+              //颜色
+              color: Color.fromARGB(255, 255, 192, 203),
+              //横线： lineThrough删除线 overline居上  underline下划线
+              decoration: TextDecoration.lineThrough ,
+              //横线样式： solid单横线  double双横线 dotted点线 dashed虚线 wavy正弦曲线
+              decorationStyle: TextDecorationStyle.wavy,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+          )
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+
+
